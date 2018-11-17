70ce8f064253b502e671af086133b0d55aff1232
Piotr Gródek <piotr.grodek@tooploox.com>
2018-11-17 18:54:17 +0100

    Missing files and tileset for buidling base

--- frontend/index.js
+++ frontend/index.js
@@ -11,6 +11,21 @@ app.use('/css', express.static('node_modules/gentelella/build/css/'));
 app.use('/js', express.static('node_modules/gentelella/build/js/'));
 app.use('/images', express.static('node_modules/gentelella/build/images/'));
 app.set('view engine', 'ejs');
+
+// example user
+// todo: db connection
+var user = {
+  level: 28,
+  name: "Kitty Katze",
+  guild: "Red Cats",
+  kmp: 120,
+  max_kmp: 250,
+};
+
 app.get('/', function (req, res) {
-  res.render('pages/index');
+  res.render('pages/index', {content: 'main', user: user});
+});
+
+app.get('/area', function(req, res) {
+  res.render('pages/index', {content: 'area', user: user});
 });
---
+++ frontend/public/gamejs/map.js
@@ -0,0 +1,25 @@
+
+
+function getTile(i) {
+    return [(i % 14) * -32, Math.floor(i / 14) * -32];
+}
+
+function generate_base(name, map, width) {
+    var body = document.getElementsByClassName(name)[0];
+    var tbl = document.createElement('table');
+    var tbdy = document.createElement('tbody');
+    for (var i = 0; i < map.length; i++) {
+        var tr = document.createElement('tr');
+        for (var j = 0; j < map[i].length; j++) {
+            var td = document.createElement('td');
+            td.classList.add('tileset');
+            var coords = getTile(map[i][j]);
+            td.setAttribute('style', 'width:32px;height:32px;background-position:' + coords[0] + 'px ' + coords[1]  + 'px');
+            td.appendChild(document.createTextNode('\u0020'));
+            tr.appendChild(td);
+        }
+        tbdy.appendChild(tr);   
+    }
+    tbl.appendChild(tbdy);
+    body.appendChild(tbl);
+}
