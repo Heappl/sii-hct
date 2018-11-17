57ba8c71f0473af4d44840fb8c87fbc867b9dcae
Piotr Gródek <piotr.grodek@tooploox.com>
2018-11-17 15:38:43 +0100

    Working on layout concept

--- frontend/index.js
+++ frontend/index.js
@@ -6,6 +6,10 @@ var server = require('http').Server(app);
 
 server.listen(8080);
 app.use(express.static('public'));
+app.use('/vendors', express.static('node_modules/gentelella/vendors/'));
+app.use('/css', express.static('node_modules/gentelella/build/css/'));
+app.use('/js', express.static('node_modules/gentelella/build/js/'));
+app.use('/images', express.static('node_modules/gentelella/build/images/'));
 app.set('view engine', 'ejs');
 app.get('/', function (req, res) {
   res.render('pages/index');
