8004ee0c6a2b5f0116d025ae3b4a149cda41d10f
Bartosz Kalinczuk <bartosz.kalinczuk@tooploox.com>
2018-11-17 18:47:29 +0100

    some objects and things on the map, and style

--- AndroidJogQuest/app/src/main/java/com/example/androidjogquest/MapsActivity.java
+++ AndroidJogQuest/app/src/main/java/com/example/androidjogquest/MapsActivity.java
@@ -5,38 +5,56 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.res.Resources;
+import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
+import android.view.animation.Animation;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.GroundOverlay;
 import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MapsActivity extends RightHelper implements OnMapReadyCallback {
+public class MapsActivity extends RightHelper implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
+    private SupportMapFragment mapFragment;
     private GoogleMap mMap;
     private Location mLastLocation = null;
     private List<SpaceJunk> junks = new ArrayList<SpaceJunk>();
-    final double viewDistance = 0.01;
+    final double viewDistanceLat = 0.014;
+    final double viewDistanceLong = 0.02;
 
     private List<Location> path = new ArrayList<>();
 
     MapsActivity() {
-        junks.add(new SpaceJunk());
+        junks.add(new SpaceJunk("SkyTower",
+                new LatLng(51.089666308, 17.017166598),
+                0.003, 0.005, "galaxy_0"));
+        junks.add(new SpaceJunk("junkyard",
+                new LatLng(51.059666308, 17.007166598),
+                0.0005, 0.0007, "junk_ship_blue"));
+        junks.add(new SpaceJunk("junkyard",
+                new LatLng(51.059666308, 17.027166598),
+                0.0005, 0.0007, "junk_ship_red"));
+        junks.add(new SpaceJunk("base",
+                new LatLng(51.099366308, 17.017466598),
+                0.0005, 0.0007, "satellite"));
     }
 
     @Override
@@ -44,7 +62,7 @@ public class MapsActivity extends RightHelper implements OnMapReadyCallback {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
@@ -87,29 +105,45 @@ public class MapsActivity extends RightHelper implements OnMapReadyCallback {
     Location getLast() {
         return path.get(path.size() - 1);
     }
+    LatLng getLastLatLng() {
+        return new LatLng(getLast().getLatitude(), getLast().getLongitude());
+    }
+
+    LatLngBounds getLastBounds() {
+        LatLng min = new LatLng(getLast().getLatitude() - 0.0003, getLast().getLongitude() - 0.0005);
+        LatLng max = new LatLng(getLast().getLatitude() + 0.0003, getLast().getLongitude() + 0.0005);
+        return new LatLngBounds(min, max);
+    }
 
     void setCamera() {
         Location last = getLast();
-        LatLng from = new LatLng(last.getLatitude() - viewDistance, last.getLongitude() - viewDistance);
-        LatLng to = new LatLng(last.getLatitude() + viewDistance, last.getLongitude() + viewDistance);
+        LatLng from = new LatLng(last.getLatitude() - viewDistanceLong, last.getLongitude() - viewDistanceLong);
+        LatLng to = new LatLng(last.getLatitude() + viewDistanceLat, last.getLongitude() + viewDistanceLat);
         LatLngBounds bounds = new LatLngBounds(from, to);
         mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 30));
     }
 
     void drawPath() {
-        PolylineOptions polyline = new PolylineOptions();
+        PolylineOptions polyline = new PolylineOptions().color(R.color.colorPath);
         for (Location loc : path) {
             polyline.add(new LatLng(loc.getLatitude(), loc.getLongitude()));
         }
         mMap.addPolyline(polyline);
+        mMap.addMarker(new MarkerOptions()
+                .position(getLastLatLng())
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ship_small)));
     }
 
     void drawNearbyObjects() {
         for (SpaceJunk junk : junks) {
-            if (junk.getDistance(getLast()) < viewDistance) {
+            if (junk.getDistance(getLast(), viewDistanceLat, viewDistanceLong)) {
+                String imageName = junk.getImageName();
+                Resources resources = getResources();
+                final int resourceId = resources.getIdentifier(imageName, "drawable", getPackageName());
                 mMap.addGroundOverlay(new GroundOverlayOptions()
                         .positionFromBounds(junk.getBounds())
-                        .image(BitmapDescriptorFactory.fromResource(R.drawable.galaxy_0)));
+                        .image(BitmapDescriptorFactory.fromResource(resourceId))
+                        .clickable(true));
             }
         }
     }
@@ -120,8 +154,9 @@ public class MapsActivity extends RightHelper implements OnMapReadyCallback {
             mMap.clear();
 
             drawPath();
+            if (path.size() < 2)
+                setCamera();
             drawNearbyObjects();
-            setCamera();
         }
     }
 
@@ -138,6 +173,7 @@ public class MapsActivity extends RightHelper implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         Log.i("Main", "onMapReady");
         mMap = googleMap;
+        mMap.setOnMarkerClickListener(this);
 
         try {
             // Customise the styling of the base map using a JSON object defined
@@ -153,5 +189,11 @@ public class MapsActivity extends RightHelper implements OnMapReadyCallback {
             Log.e("MapsActivityRaw", "Can't find style.", e);
         }
         drawThings();
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        setCamera();
+        return false;
     }
 }
--- AndroidJogQuest/app/src/main/java/com/example/androidjogquest/SpaceJunk.java
+++ AndroidJogQuest/app/src/main/java/com/example/androidjogquest/SpaceJunk.java
@@ -2,28 +2,44 @@ package com.example.androidjogquest;
 
 import android.location.Location;
 
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 
 public class SpaceJunk {
+    String name, imageName;
+    LatLng position;
+    double latSize;
+    double longSize;
 
+    SpaceJunk(String name, LatLng position, double latSize, double longSize, String resourceName) {
+        this.name = name;
+        this.position = position;
+        this.latSize = latSize;
+        this.longSize = longSize;
+        this.imageName = resourceName;
+    }
 
-    public double getDistance(Location last) {
-        return 0;
+    public String getImageName() {
+        return this.imageName;
+    }
+
+    public boolean getDistance(Location last, double latDist, double longDist) {
+        return (position.latitude - last.getLatitude() < latDist)
+                && (position.longitude - last.getLongitude() < longDist);
     }
 
     public LatLngBounds getBounds() {
-        LatLng skytower_min = new LatLng(51.086666308, 17.014166598);
-        LatLng skytower_max = new LatLng(51.092666308, 17.020166598);
+        LatLng skytower_min = new LatLng(position.latitude - latSize, position.longitude - longSize);
+        LatLng skytower_max = new LatLng(position.latitude + latSize, position.longitude + longSize);
         return new LatLngBounds(skytower_min, skytower_max);
     }
 
     public LatLng getPosition() {
-        LatLng skytower = new LatLng(51.089666308, 17.017166598);
-        return skytower;
+        return position;
     }
 
     public String getTitle() {
-        return "SkyTower";
+        return name;
     }
 }
